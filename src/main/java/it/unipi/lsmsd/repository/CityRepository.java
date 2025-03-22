c08d396fdc43a955a2a972139dd92b7afe06292b
lorenzo-vezzani <lorenzovezzani8@gmail.com>
2025-03-22 19:11:24 +0100

    add favorite-cities functionalities

--- src/main/java/it/unipi/lsmsd/repository/CityRepository.java
+++ src/main/java/it/unipi/lsmsd/repository/CityRepository.java
@@ -1,5 +1,6 @@
 package it.unipi.lsmsd.repository;
 
+<<<<<<< HEAD
 import it.unipi.lsmsd.model.City;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -10,3 +11,12 @@ import java.util.Optional;
 public interface CityRepository extends MongoRepository<City, String> {
     Optional<City> findByName(String name);
 }
+=======
+import org.springframework.data.mongodb.repository.MongoRepository;
+import it.unipi.lsmsd.model.City;
+import java.util.Optional;
+
+public interface CityRepository extends MongoRepository<City, String> {
+    Optional<City> findByName(String name);
+}
+>>>>>>> 0fec31d (add favorite-cities functionalities)
