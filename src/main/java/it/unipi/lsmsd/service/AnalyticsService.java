5744298b38226377cb58a416226777f6b4829552
Rojan Shrestha <rojan@trustwaresolutions.com>
2025-04-28 15:44:16 +0200

    * Added more Analytics Queries * Updated AnalyticsService to use mongoClient from configuration file

--- src/main/java/it/unipi/lsmsd/service/AnalyticsService.java
+++ src/main/java/it/unipi/lsmsd/service/AnalyticsService.java
@@ -13,10 +13,13 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 import it.unipi.lsmsd.model.ExtremeWeatherEventCategory;
 import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -26,29 +29,35 @@ import java.util.stream.StreamSupport;
 @Service
 public class AnalyticsService{
 
-    private final MongoClient mongoClient;
-    private final MongoDatabase database;
-    private final MongoCollection<Document> eweCollection;
-    private final MongoCollection<Document> measurementCollection;
-    private final MongoCollection<Document> cityCollection;
-
-    public AnalyticsService() {
-        this.mongoClient = MongoClients.create(
-                MongoClientSettings.builder()
-                        .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
-                        .build()
-        );
-        this.database = mongoClient.getDatabase("WeatherApp");
-        this.eweCollection = database.getCollection("extreme_weather_events");
-        this.measurementCollection = database.getCollection("hourly_measurements");
-        this.cityCollection = database.getCollection("cities");
+	@Autowired
+    private MongoClient mongoClient;
+	
+	private MongoDatabase getDatabase() {
+        return mongoClient.getDatabase("WeatherApp");
     }
+    // private final MongoDatabase database;
+    // private final MongoCollection<Document> eweCollection;
+    // private final MongoCollection<Document> measurementCollection;
+    // private final MongoCollection<Document> cityCollection;
+
+    // public AnalyticsService() {
+    //     this.mongoClient = MongoClients.create(
+    //             MongoClientSettings.builder()
+    //                     .applyConnectionString(new ConnectionString("mongodb://localhost:27017"))
+    //                     .build()
+    //     );
+    //     this.database = mongoClient.getDatabase("WeatherApp");
+    //     this.eweCollection = database.getCollection("extreme_weather_events");
+    //     this.measurementCollection = database.getCollection("hourly_measurements");
+    //     this.cityCollection = database.getCollection("cities");
+    // }
 
     public List<Document> topCitiesMostAffectedByEwe(
             int maxNumCitiesToFind,
             ExtremeWeatherEventCategory EweCategory
     ){
-        return StreamSupport.stream(eweCollection.aggregate(
+		MongoCollection<Document> collection = getDatabase().getCollection("extreme_weather_events");
+        return StreamSupport.stream(collection.aggregate(
                 Arrays.asList(
                         // select only the EWEs of the given category
                         match(
@@ -79,7 +88,8 @@ public class AnalyticsService{
             LocalDateTime startDate,
             LocalDateTime endDate
     ){
-        return StreamSupport.stream(eweCollection.aggregate(
+		MongoCollection<Document> collection = getDatabase().getCollection("extreme_weather_events");
+        return StreamSupport.stream(collection.aggregate(
                 Arrays.asList(
                         match(and(
                                 // select only the EWEs of the given category
@@ -115,7 +125,8 @@ public class AnalyticsService{
         Date start = Date.from(startDate.toInstant(ZoneOffset.UTC));
         Date end = Date.from(endDate.toInstant(ZoneOffset.UTC));
 
-        return StreamSupport.stream(measurementCollection.aggregate(
+		MongoCollection<Document> collection = getDatabase().getCollection("hourly_measurements");
+        return StreamSupport.stream(collection.aggregate(
             Arrays.asList(
                     // select only the ones where the date is between start and end
                     match(and(
@@ -138,4 +149,81 @@ public class AnalyticsService{
             )).spliterator(), false)
             .collect(Collectors.toList());
     }
+
+	 // Average Temperature per City for Last 30 Days with City Info
+    public List<Document> getAvgTemperaturePerCityLast30Days() {
+
+        // Get the current time in UTC
+        ZonedDateTime thirtyDaysAgoUTC = ZonedDateTime.now(ZoneOffset.UTC).minusDays(30);
+        Date thirtyDaysAgo = Date.from(thirtyDaysAgoUTC.toInstant());
+
+        // Get the collection
+        MongoCollection<Document> collection = getDatabase().getCollection("hourly_measurements");
+
+        // Aggregation pipeline
+        List<Bson> pipeline = List.of(
+                match(gte("time", thirtyDaysAgo)),  // Match documents from the last 30 days
+                group("$cityId", avg("avgTemperature", "$temperature")),  // Group by cityId and calculate average temperature
+                lookup("cities", "_id", "_id", "cityDetails"),  // Lookup city details
+                unwind("$cityDetails"),  // Unwind the cityDetails array
+                project(fields(
+                        include("avgTemperature"),
+                        computed("cityName", "$cityDetails.name"),
+                        computed("cityRegion", "$cityDetails.region")
+                ))
+        );
+
+        // Execute aggregation
+        return collection.aggregate(pipeline).into(new java.util.ArrayList<>());
+    }
+
+    // Hottest Day for each city
+    public List<Document> getHottestDayPerCity() {
+
+        // Get the collection
+        MongoCollection<Document> collection = getDatabase().getCollection("hourly_measurements");
+
+        // Aggregation pipeline
+        List<Bson> pipeline = List.of(
+                group("$cityId", max("maxTemperature", "$temperature")),  // Group by cityId and pick max temperature
+                lookup("cities", "_id", "_id", "cityDetails"),  // Lookup city details
+                unwind("$cityDetails"),  // Unwind the cityDetails array
+                project(fields(
+                        include("maxTemperature"),
+                        computed("cityName", "$cityDetails.name"),
+                        computed("cityRegion", "$cityDetails.region")
+                ))
+        );
+
+        // Execute aggregation
+        return collection.aggregate(pipeline).into(new java.util.ArrayList<>());
+    }
+
+    // Total Rainfall per City in Last 30 Days
+    public List<Document> getTotalRainfallPerCityLast30Days() {
+
+        // Get the current time in UTC
+        ZonedDateTime thirtyDaysAgoUTC = ZonedDateTime.now(ZoneOffset.UTC).minusDays(30);
+        Date thirtyDaysAgo = Date.from(thirtyDaysAgoUTC.toInstant());
+
+        // Get the collection
+        MongoCollection<Document> collection = getDatabase().getCollection("hourly_measurements");
+
+        // Aggregation pipeline
+        List<Bson> pipeline = List.of(
+                match(gte("time", thirtyDaysAgo)),  // Match documents from the last 30 days
+                group("$cityId", sum("totalRainfall", "$rainfall")),  // Group by cityId and sum rainfall
+                lookup("cities", "_id", "_id", "cityDetails"),  // Lookup city details
+                unwind("$cityDetails"),  // Unwind the cityDetails array
+                project(fields(
+                        include("totalRainfall"),
+                        computed("cityName", "$cityDetails.name"),
+                        computed("cityRegion", "$cityDetails.region")
+                ))
+        );
+
+        // Execute aggregation
+        return collection.aggregate(pipeline).into(new java.util.ArrayList<>());
+    }
+
 }
